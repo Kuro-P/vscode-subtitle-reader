1769e8358deff935a820c4e53b2f87e8eac438ad
kuro-p <837408039@qq.com>
2023-05-17 17:05:01 +0800

    chore: refactor pannel instance

--- src/preview/index.ts
+++ src/preview/index.ts
@@ -5,112 +5,103 @@ import { promises as fsPromises } from "fs";
 import { extractAssInfo } from './ass';
 import { extractSrtInfo } from './srt';
 import * as Handlebars from 'handlebars';
+import { Panel } from './instance';
+import { context } from './../extension';
 
+export let panel: Panel;
 
-class File {
-  constructor() {
+export async function displayPreviewPanel(): Promise<Panel> {
+  const previewPanel = createPreviewPanel({
+    localResourceRoots: [ context.extensionUri ]
+  });
+  const content = await generateHTML(previewPanel);
+  previewPanel.webview.html = content;
 
-  }
+  panel = new Panel(previewPanel);
+
+  return panel;
 }
 
+export function createPreviewPanel(
+  options: {
+    viewType?: string,
+    title?: string,
+    viewColumn?: vscode.ViewColumn,
+    localResourceRoots: vscode.Uri[]
+  }): vscode.WebviewPanel {
 
-class PreviewPanel {
-  // create
-  constructor() {
+  const {
+    viewType = "subtitlePreview",
+    title = "Subtitle preview pannel",
+    viewColumn = vscode.ViewColumn.Beside,
+    localResourceRoots
+  } = options;
 
-  }
-
-  open() {
-
-  }
-
-  close() {
-
-  }
-
-  reload() {
-
-  }
-
-
-}
-
-// 对文档内容进行加工 如 提取有用的信息
-// 主要根据 文件格式 对字幕对话内容进行统一处理 方便 preview panel 进行展示
-export function processContent(document: vscode.TextDocument, context: vscode.ExtensionContext) {
-  const fileText = document.getText();
-  const fileName = document.fileName.split('/').at(-1) || '';
-  const languageId = document.languageId;
-
-  console.log('lineCount', document.lineCount);
-  let contentInstance;
-
-  if (isSSA(languageId) || isASS(languageId)) {
-    contentInstance = extractAssInfo(fileText);
-  } else if (isSRT(languageId)) {
-    contentInstance = extractSrtInfo(fileText);
-  }
-
-  openPreviewPanel(fileName, context, contentInstance);
-}
-
-export function openPreviewPanel(fileName: string, context: vscode.ExtensionContext, contentInstance: any) {
-  // ✅ TODO 1. get html raw file
-  // ✅ TODO 2. get needed info from .ass file
-  // ✅ TODO 3. render html with needed info
-  // ✅ TODO 4. render to panel
-  // ✅ TODO 5. load panel style
-  // TODO 6. design panel style
-
-  const _extensionUri = context.extensionUri;
-  const _extensionPath = context.extensionPath;
-
-  // 调起 webview 模板
   const webviewPanel = vscode.window.createWebviewPanel(
     // id
-    "subtitlePreview",
+    viewType,
     // name
-    "Subtitle preview pannel",
+    title,
     {
       preserveFocus: true,
-      viewColumn: vscode.ViewColumn.Beside
+      viewColumn
     },
     {
       enableCommandUris: false,
       enableScripts: true,
       enableFindWidget: true,
       retainContextWhenHidden: false,
-      localResourceRoots: [ context.extensionUri ]
+      localResourceRoots: localResourceRoots
     }
   );
 
+  return webviewPanel;
+}
+
+export async function generateHTML(webviewPanel: vscode.WebviewPanel): Promise<string> {
+  const document = vscode.window.activeTextEditor?.document;
+  if (!document) {
+    vscode.window.showErrorMessage('Not found activated tab');
+    return '';
+  }
+
+  const fileText = document.getText();
+  const fileName = document.fileName.split('/').at(-1) || '';
+  const languageId = document.languageId;
+  let contentInstance: any;
+
+  if (isSSA(languageId) || isASS(languageId)) {
+    contentInstance = extractAssInfo(fileText);
+  } else if (isSRT(languageId)) {
+    contentInstance = extractSrtInfo(fileText);
+  }
   if (!contentInstance) {
-    webviewPanel.webview.html = '<h3>请检查文件格式是否正确</h3>';
-    return;
+    return '<h3>请检查文件格式是否正确</h3>';
   }
 
   // generate webview HTML
   try {
-    fsPromises.readFile(path.join(_extensionPath, '/src/assets/index.html'), { encoding: 'utf-8' }).then((panelTempl) => {
+    const { extensionUri, extensionPath } = context;
+    const panelTempl = await fsPromises.readFile(path.join(extensionPath, '/src/assets/index.html'), { encoding: 'utf-8' });
+    const styleUri = webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'dist', 'main.css'));
+    const scriptUri = webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'dist', 'main.js'));
 
-      const styleUri = webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(_extensionUri, 'dist', 'main.css'));
-      const scriptUri = webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(_extensionUri, 'dist', 'main.js'));
-
-      const panelParams = Object.assign({}, contentInstance, {
-        fileName,
-        styleUri: styleUri,
-        scriptUri: scriptUri,
-        cspSource: webviewPanel.webview.cspSource,
-        nonce: getNonce(),
-      });
-
-      const template = Handlebars.compile(panelTempl);
-      const panelContent = template(panelParams);
-
-      webviewPanel.webview.html = panelContent;
+    const panelParams = Object.assign({}, contentInstance, {
+      fileName,
+      styleUri: styleUri,
+      scriptUri: scriptUri,
+      cspSource: webviewPanel.webview.cspSource,
+      nonce: getNonce(),
     });
 
+    const template = Handlebars.compile(panelTempl);
+    const panelContent = template(panelParams);
+    return panelContent;
+
   } catch (e) {
-    console.log(e);
+    return `
+      <h3>Ops！出错了</h3>
+      <p>${ e }</p>
+    `;
   }
 }
